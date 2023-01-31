acb21d49a24a17169c233255029604a6c957023c
RavilesX <ricardo.aviles.sanders@gmail.com>
2023-01-30 23:20:56 -0600

    adding grading students execirse and deleting comments on time conversion program

--- timeConversion.py
+++ timeConversion.py
@@ -4,12 +4,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'diagonalDifference' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#a
 s="12:05:45AM"
 def timeConversion(s):
     hours=int(s[0:2])
