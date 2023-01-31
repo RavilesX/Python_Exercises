7449f1e35199563dc18db5eee014ae0f1be900f7
RavilesX <ricardo.aviles.sanders@gmail.com>
2023-01-30 22:36:02 -0600

    Adding exercise files to my repository

---
+++ programa.py
@@ -0,0 +1,43 @@
+
+import tkinter
+from tkinter import messagebox
+
+#def boton_de_ventana():
+ #   respuesta=messagebox.askyesno("Pregunta","Te gusta mi programa")
+  #  back=cadena.get()    
+   # for i in range(0,len(back):
+        
+        
+        
+ #   print(new_string)
+    
+#def segundo_boton():
+#    pass
+
+
+#mi_ventana = tkinter.Tk()
+ 
+#mi_ventana.geometry("640x480")
+
+#cadena=tkinter.Entry()
+#cadena.pack()
+
+
+#Creo un boton
+#mi_boton=tkinter.Button(text="Cerrar",command=boton_de_ventana)
+
+
+#segundo_boton=tkinter.Button(text="Otro boton",command=segundo_boton)
+
+#Coloco mi boton en la ventana principal
+#mi_boton.pack()
+
+
+
+#Se muestra la ventana principal
+#mi_ventana.mainloop()
+
+
+#!/bin/python3
+
+#!/bin/python3
---
+++ timeConversion.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#a
+s="12:05:45AM"
+def timeConversion(s):
+    hours=int(s[0:2])
+    #print(format(hour,'2f'))
+    if s.__contains__("PM"):
+        if hours!=12:
+            hours=hours+12
+    else:
+        if hours==12:
+            hours=0    
+    military=f"{str(hours).zfill(2)}"+s[2:8]
+    return military
+
+        
+result = timeConversion(s)
+print(result)
